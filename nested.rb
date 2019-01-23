246a7dc4ef6e2ce61cf1a65f1ada33bb482990f3
Oliver <oliver.grant@gmail.com>
2019-01-23 22:09:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -16,12 +17,12 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
+     binding.pry
 end
 
 def adding_matz
